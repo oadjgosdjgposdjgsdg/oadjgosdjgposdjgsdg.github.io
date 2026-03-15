76965bc2db381472ba9ee0ef1edb1af4434b610c
oadjgosdjgposdjgsdg <projected.server.side@gmail.com>
2026-03-15 22:20:50 +0100

    Update base.js

--- components/base.js
+++ components/base.js
@@ -1,12 +1,12 @@
 // Load Navbar
-fetch("components/navbar.html")
+fetch("../components/navbar.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   });
 
 // Load Footer
-fetch("components/footer.html")
+fetch("../components/footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
